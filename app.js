0d2f5566b4068feeacbbba8bcba7a858f42d2f39
carola1408 <118614918+carola1408@users.noreply.github.com>
2023-11-07 09:03:23 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // Include packages and define server related variables
 const express = require('express')
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars') // 引入 express-handlebars
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const app = express()
 const PORT = 3000
+
+// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+
+// 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
