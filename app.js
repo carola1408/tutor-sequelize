101dbb297f863df4dcb4eb4cc70dbb7d846be0d0
carola1408 <118614918+carola1408@users.noreply.github.com>
2023-11-07 11:18:26 +0800

    feat: add 安裝並設定 Flash Message

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // Include packages and define server related variables
 const express = require('express')
 const exphbs = require('express-handlebars') // 引入 express-handlebars
+const flash = require('connect-flash') //自訂訊息
+const session = require('express-session') //使用 session
 const methodOverride = require('method-override')
 const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
-
+const SESSION_SECRET = 'secret'
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 
@@ -15,7 +17,13 @@ app.set('view engine', 'hbs')
 //使用 body-parser
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages')  // 設定 warning_msg 訊息
+  next()
+})
 
 app.use(routes)
 app.listen(PORT, () => {
