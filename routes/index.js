1cbc886804af077a9145b1f3cdd59d9c00e6e6ad
carola1408 <118614918+carola1408@users.noreply.github.com>
2023-11-07 09:51:07 +0800

    feat: add main.hbs signup.hbs and signin.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const router = express.Router()
+//首頁
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//登入入口
+router.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+//註冊入口
+router.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+//登出入口
+router.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 module.exports = router
