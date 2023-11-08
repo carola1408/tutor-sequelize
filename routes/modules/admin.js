eb40244847cd186f7e5c631c44d4cbe9683ccb0c
carola1408 <118614918+carola1408@users.noreply.github.com>
2023-11-08 10:15:41 +0800

    feat: add 使用者清單頁面

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
+// controllers
 const adminController = require('../../controllers/admin-controller')
+// middleware
 const { authenticatedAdmin } = require('../../middleware/auth')  //引入 auth.js
+// admin home
 router.get('/home', authenticatedAdmin, adminController.getHome)
+// admin users
 router.use('/', (req, res) => res.redirect('/admin/home'))
 module.exports = router
